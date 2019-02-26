eea8448b0d02540f5a00f6e963b261017a99a94f
risenforces <risenforces@gmail.com>
2019-02-26 18:16:22 +0500

    add clustering move port to .env

--- app/index.js
+++ app/index.js
@@ -1,8 +1,45 @@
+require("dotenv").config()
+
+const os = require("os")
+const cluster = require("cluster")
 const fastify = require("./fastify")
 
 require("./routes")
 
-fastify.listen(8080, (err, address) => {
-  if (err) throw err
-  console.info(`Listening on ${address}`)
-})
+const port = parseInt(process.env.PORT, 10) || 8080
+const clusteringEnabled = process.env.CLUSTERING_ENABLED === "true"
+
+const setupClusters = () => {
+  const cores = os.cpus().length
+
+  console.info(`Setting on ${cores} cores.`)
+
+  for (let i = 0; i < cores; i++) cluster.fork()
+
+  cluster.on("online", (worker) => {
+    console.info(`Worker ${worker.process.pid} is started.`)
+  })
+
+  cluster.on("message", (worker, message) => {
+    console.info(`Worker ${worker.process.pid} sent message: ${message}.`)
+  })
+
+  cluster.on("exit", (worker, code, signal) => {
+    const message = `Worker ${worker.process.pid} died with code ${code} and signal ${signal}.`
+    fastify.log.error(message)
+    console.error(message)
+  })
+}
+
+const setupFastify = () => {
+  fastify.listen(port, (err, address) => {
+    if (err) throw err
+    console.info(`Listening on ${address}.`)
+  })
+}
+
+if (clusteringEnabled && cluster.isMaster) {
+  setupClusters()
+} else {
+  setupFastify()
+}
