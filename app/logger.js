843b8c79e7833fffca015d7e387cbdc2ea452b7b
risenforces <risenforces@gmail.com>
2019-02-27 03:03:13 +0500

    make logger urls shorter in dev mode

--- app/logger.js
+++ app/logger.js
@@ -6,6 +6,10 @@ const dev = process.env.NODE_ENV !== "production"
 const morganFormat = dev ? "dev" : "common"
 const morganOptions = dev ? {} : { stream: fs.createWriteStream("./app.log") }
 
+if (dev) {
+  morgan.token("url", req => (req.url.length < 49 ? req.url : `${req.url.slice(0, 47)}...`))
+}
+
 const logger = morgan(morganFormat, morganOptions)
 
 module.exports = logger
