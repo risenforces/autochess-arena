2d0a4455b2bae13d58a5cc70034b9d39c9385024
risenforces <risenforces@gmail.com>
2019-03-02 23:33:25 +0500

    fix styled-components & next js issue

--- app/pages/_document.js
+++ app/pages/_document.js
@@ -1,36 +1,20 @@
-import Document, { Head, Main, NextScript } from "next/document"
+import Document from "next/document"
 import { ServerStyleSheet } from "styled-components"
 
 export default class CustomDocument extends Document {
-  static getInitialProps({ renderPage }) {
+  static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet()
 
-    const page = renderPage(App => props =>
-      sheet.collectStyles(<App {...props} />)
-    )
+    const originalRenderPage = ctx.renderPage
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
+      })
 
-    const styleTags = sheet.getStyleElement()
-
-    return { ...page, styleTags }
-  }
-
-  render() {
-    return (
-      <html>
-        <Head>
-          <link rel="shortcut icon" href="/static/favicon.ico" />
-          <meta
-            name="viewport"
-            content="initial-scale=1.0, width=device-width, maximum-scale=1.0"
-            key="viewport"
-          />
-          {this.props.styleTags}
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </html>
-    )
+    const initialProps = await Document.getInitialProps(ctx)
+    return {
+      ...initialProps,
+      styles: [...initialProps.styles, ...sheet.getStyleElement()]
+    }
   }
 }
