62927cbd16cf743a3a13a24f8be0fabe07ba1155
risenforces <risenforces@gmail.com>
2019-03-08 22:11:20 +0500

    disable clustering due to project specialty

--- server/index.js
+++ server/index.js
@@ -1,7 +1,5 @@
 require("dotenv").config()
 
-const os = require("os")
-const cluster = require("cluster")
 const app = require("../app")
 const server = require("./server")
 const { dev, port } = require("./constants/env")
@@ -14,38 +12,7 @@ const info = message => {
   if (!dev) console.info(message)
 }
 
-const setupClusters = () => {
-  const cores = os.cpus().length
-
-  info(`Setting on ${cores} cores.`)
-
-  for (let i = 0; i < cores; i++) cluster.fork()
-
-  cluster.on("online", worker => {
-    info(`Worker ${worker.process.pid} is started.`)
-  })
-
-  cluster.on("message", (worker, message) => {
-    info(`Worker ${worker.process.pid} sent message: ${message}.`)
-  })
-
-  cluster.on("exit", (worker, code, signal) => {
-    info(
-      `Worker ${
-        worker.process.pid
-      } died with code ${code} and signal ${signal}.`
-    )
-  })
-}
-
-const setupApp = async () => {
-  await app.prepare()
+app.prepare().then(() => {
   server.listen(port)
   info(`Listening on ${port}`)
-}
-
-if (!dev && cluster.isMaster) {
-  setupClusters()
-} else {
-  setupApp()
-}
+})
